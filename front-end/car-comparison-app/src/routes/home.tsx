e60a553758731ee8377a6a6099159afa0250d2bb
P311 <linhao.liu98@gmail.com>
2025-03-29 16:25:27 -0400

    front-end: install shadcn

--- front-end/car-comparison-app/src/routes/home.tsx
+++ front-end/car-comparison-app/src/routes/home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Button } from "@/components/ui/button"
 
 function Home() {
-  return (<h1 className="text-3xl font-bold underline">Home</h1>);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default Home;
