a80e4c9fc446b85878b10a39adb2af80fa53fbbb
P311 <linhao.liu98@gmail.com>
2025-03-29 15:58:02 -0400

    front-end: install tailwind css

--- front-end/car-comparison-app/src/main.tsx
+++ front-end/car-comparison-app/src/main.tsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
   {path: '/', element: <Home />},
 ])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
