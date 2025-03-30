ffabc3a85f4aaf8b9b6401d0d6e118b72e14e08d
P311 <linhao.liu98@gmail.com>
2025-03-29 21:49:01 -0400

    front-end home: implement initial home page

--- front-end/car-comparison-app/src/main.tsx
+++ front-end/car-comparison-app/src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import './styles/globals.css'
+import "./styles/globals.css";
 import Home from "./routes/home";
 
-const router = createBrowserRouter([
-  {path: '/', element: <Home />},
-])
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- front-end/car-comparison-app/src/routes/home.tsx
+++ front-end/car-comparison-app/src/routes/home.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="text-2xl font-bold">Hello World</h1>
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center h-screen bg-linear-to-r from-cyan-500 to-blue-500">
+      <span className="text-7xl font-bold mb-10 text-white/85">
+        Find Your Perfect Car Match
+      </span>
+      <span className="text-2xl text-white/85 mb-20 w-230 text-center">
+        Compare specs, prices, and reviews from multiple sources to make the
+        best informed decision for your next vehicle purchase.
+      </span>
+      <div className="flex flex-row gap-80">
+        <Button
+          className="bg-cyan-300 w-40 h-12 text-sky-800 font-bold text-lg hover:bg-cyan-500 hover:text-white"
+          onClick={() => (location.href = "/register")}
+        >
+          Register
+        </Button>
+        <Button
+          className="bg-cyan-300 w-40 h-12 text-sky-800 font-bold text-lg hover:bg-cyan-500 hover:text-white"
+          onClick={() => (location.href = "/login")}
+        >
+          Login
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
